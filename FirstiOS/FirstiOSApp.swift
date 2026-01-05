cb329255a725e5edb5b239670ec787b0e7ea890b
Daniil Kozar <daqua.998@gmail.com>
2026-01-05 15:55:33 -0700

    Initial Commit

---
+++ FirstiOS/FirstiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstiOSApp.swift
+//  FirstiOS
+//
+//  Created by Dan on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
